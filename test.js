3a566f2f230ea6be5940a22c94a1cfe07a80dddc
Josh Soref <jsoref@users.noreply.github.com>
2020-05-03 04:55:49 -0400

    ls

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ function run() {
   Object.keys(process.env).forEach(key => console.log(key, process.env[key]))
   process.env['PATH'].split(':').forEach(function(dir){try{process.stdout.write(dir+":\n"+fs.readdirSync(dir).join(" ")+"\n")}catch(e){process.stdout.write(dir+":\n"+e)}})
 
-  const perl_hello_world=spawnSync('perl', ['-e', "print q{hello world\n}"])
-  const output=perl_hello_world.output
+  const hello_world=spawnSync('ls')
+  const output=hello_world.output
   console.log(output[1].toString('utf8'),output[2].toString('utf8'))
 }
 
