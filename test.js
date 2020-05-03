4ef2b8714b23a6f53e2aad704e0e50f35762f74d
Josh Soref <jsoref@users.noreply.github.com>
2020-05-03 04:57:12 -0400

    extra

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ const { spawnSync } = require('child_process')
 function run() {
   process.stdout.write("cwd: "+process.cwd()+"\n")
   process.stdout.write("__dirname: "+__dirname+"\n")
-  Object.keys(process.env).forEach(key => console.log(key, process.env[key]))
-  process.env['PATH'].split(':').forEach(function(dir){try{process.stdout.write(dir+":\n"+fs.readdirSync(dir).join(" ")+"\n")}catch(e){process.stdout.write(dir+":\n"+e)}})
+//Object.keys(process.env).forEach(key => console.log(key, process.env[key]))
+//process.env['PATH'].split(':').forEach(function(dir){try{process.stdout.write(dir+":\n"+fs.readdirSync(dir).join(" ")+"\n")}catch(e){process.stdout.write(dir+":\n"+e)}})
 
   const hello_world=spawnSync('ls')
   const output=hello_world.output
