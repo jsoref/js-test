3e89b56a340b9e7c94fa88d8ef9752d6077d3b86
Josh Soref <jsoref@users.noreply.github.com>
2020-05-03 04:53:31 -0400

    where am i

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const { spawnSync } = require('child_process')
 function run() {
+  process.stdout.write("cwd: "+process.cwd()+"\n")
+  process.stdout.write("__dirname: "+__dirname+"\n")
   Object.keys(process.env).forEach(key => console.log(key, process.env[key]))
   process.env['PATH'].split(':').forEach(function(dir){try{process.stdout.write(dir+":\n"+fs.readdirSync(dir).join(" ")+"\n")}catch(e){process.stdout.write(dir+":\n"+e)}})
 
