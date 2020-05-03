4c2554afb086160720bd2e8c752ba1953a30bb65
Josh Soref <jsoref@users.noreply.github.com>
2020-05-03 04:37:16 -0400

    perl

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-const fs = require('fs');
+const fs = require('fs')
+const { spawnSync } = require('child_process')
 function run() {
   Object.keys(process.env).forEach(key => console.log(key, process.env[key]))
-  process.env['PATH'].split(':').forEach(dir=>fs.readdir(dir, (e,files)=>console.log(dir,files)))
+  process.env['PATH'].split(':').forEach(function(dir){try{process.stdout.write(dir+":\n"+fs.readdirSync(dir).join(" ")+"\n")}catch(e){process.stdout.write(dir+":\n"+e)}})
+
+  const perl_hello_world=spawnSync('perl', ['-e', "print q{hello world\n}"])
+  const output=perl_hello_world.output
+  console.log(output[1].toString('utf8'),output[2].toString('utf8'))
 }
 
 run()
