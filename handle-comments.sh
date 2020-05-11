562a1afad12b7cda15122b2ce3a7de913e1d852b
Josh Soref <jsoref@users.noreply.github.com>
2020-05-11 00:23:35 -0400

    files

--- handle-comments.sh
+++ handle-comments.sh
@@ -7,11 +7,13 @@ export spellchecker=${spellchecker:-/app}
 echo debug:
 cat "$GITHUB_EVENT_PATH"
 
-issue=$(cat "$GITHUB_EVENT_PATH" | jq -r .issue)
-number=$(echo "$issue" | jq -r .number)
-comment=$(cat "$GITHUB_EVENT_PATH" | jq -r .comment)
-username=$(echo "$comment" | jq -r .user.login)
-body=$(echo "$comment" | jq -r .body)
+issue=$(mktemp)
+jq -r .issue < "$GITHUB_EVENT_PATH" > $issue
+number=$(jq -r .number < $issue)
+comment=$(mktemp)
+jq -r .comment < "$GITHUB_EVENT_PATH" > $comment
+username=$(jq -r .user.login < $comment)
+body=$(jq -r .body < $comment)
 
 body_to_payload() {
   BODY="$1"
@@ -42,7 +44,7 @@ comment() {
 }
 
 if echo "$body" | grep -q "setup"; then
-  COMMENTS_URL=$(echo "$issue" | jq -r .comments_url)
+  COMMENTS_URL=$(jq -r .comments_url < $issue)
   BODY=$(mktemp)
   base="<details><summary>This is data...</summary>
 
@@ -52,19 +54,21 @@ if echo "$body" | grep -q "setup"; then
 </details>"
   echo "$base" > $BODY
   PAYLOAD=$(body_to_payload $BODY)
-  RESPONSE=$(comment "$COMMENTS_URL" "$PAYLOAD")
-  COMMENTS_URL=$(echo "$RESPONSE" | jq -r .url)
+  response=$(mktemp)
+  comment "$COMMENTS_URL" "$PAYLOAD" > $response
+  cat $response
+  COMMENTS_URL=$(jq -r .url < $response)
   echo "$base
 
 [Quote this line to tell]($COMMENTS_URL) @check-spelling-bot update whitelist" > $BODY
   PAYLOAD=$(body_to_payload $BODY)
-  RESPONSE=$(comment "$COMMENTS_URL" "$PAYLOAD" "PATCH")
-  echo "$RESPONSE"
+  comment "$COMMENTS_URL" "$PAYLOAD" "PATCH" > $response
+  cat $response
   exit 0
 fi
 trigger=$(echo "$body" | perl -ne 'print if /\@check-spelling-bot(?:\s+|:\s*)update whitelist/')
 if [ -n "$trigger" ]; then
-  comments_url=$(cat "$GITHUB_EVENT_PATH" | jq -r .repository.comments_url)
+  comments_url=$(jq -r .repository.issue_comment_url < "$GITHUB_EVENT_PATH")
   export comments_url=$(echo "$comments_url" | perl -pne 's{\{.*\}}{/\\d+}')
   comment_url=$(echo "$trigger" | perl -ne 'next unless m{($ENV{comments_url})}; print "$1\n";')
   comment=$(comment "$comment_url")
